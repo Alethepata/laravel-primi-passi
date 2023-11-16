9cf5d8c7e14c3a68a9c6624be63a0aed803ee8f1
Alethepata <alessia.patane@gmail.com>
2023-11-16 16:41:42 +0100

    [edit] menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Hello World !</title>
 </head>
 <body>
 
     <header>
-        <div class="container">
+        <div class="container my-3">
 
-            @foreach ($menu as $item)
+            <ul class="list-unstyled d-flex">
+                @foreach ($menu as $item)
 
-                <li>
-                  <a href={{$item['link']}}>{{$item['title']}}</a>
-                </li>
+                  <li>
+                    <a class="text-decoration-none me-4" href={{$item['link']}}>{{$item['title']}}</a>
+                  </li>
 
-            @endforeach
+                @endforeach
+            </ul>
 
         </div>
     </header>
