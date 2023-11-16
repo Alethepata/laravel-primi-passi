9e724694f9d40d9c8e98fc575fb3d4e88e39f09a
Alethepata <alessia.patane@gmail.com>
2023-11-16 13:50:50 +0100

    [add] name/surname

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $name = 'Kira';
+    $surname = 'The dog';
+    return view('home', compact('name', 'surname'));
 });
